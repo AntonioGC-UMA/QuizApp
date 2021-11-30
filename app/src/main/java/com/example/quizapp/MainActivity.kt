8759bd172bd9ea2fcafbfefdaa98c7a7386bf3fe
xmariia55x <mgm7cns@uma.es>
2021-11-30 20:51:08 +0100

    Eliminando fallos

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -7,60 +7,56 @@ import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
-    var email : EditText? = null
-    var password : EditText? = null
+    var emailText : EditText? = null
+    var passwordText : EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        email = findViewById<EditText>(R.id.email)
-        password = findViewById<EditText>(R.id.password)
+        emailText = findViewById<EditText>(R.id.email)
+        passwordText = findViewById<EditText>(R.id.password)
 
         findViewById<Button>(R.id.login).setOnClickListener {
-            val e = email?.text.toString()
-            val p = password?.text.toString()
-
             val auth = FirebaseAuth.getInstance()
-
-            if (!TextUtils.isEmpty(e) && !TextUtils.isEmpty(p)) {
-                auth.signInWithEmailAndPassword(e, p).addOnCompleteListener {
-                    task ->
-                                if (task.isSuccessful) {
-                                    Toast.makeText(this, "Autenticacion correcta", Toast.LENGTH_SHORT).show()
-                            val intent = Intent(this, MainActivity::class.java)
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                            startActivity(intent)
-                        } else {
-                            Toast.makeText(this, "Autenticacion fallida", Toast.LENGTH_SHORT).show()
-                        }
+            val email = emailText?.text.toString()
+            val password = passwordText?.text.toString()
+            if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                        task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Autenticacion correcta", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, Home::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "Autenticacion fallida", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
 
         findViewById<Button>(R.id.registrar).setOnClickListener {
-            val e = email?.text.toString()
-            val p = password?.text.toString()
-
             val auth = FirebaseAuth.getInstance()
-
-            if (!TextUtils.isEmpty(e) && !TextUtils.isEmpty(p)) {
-                auth.createUserWithEmailAndPassword(e, p)
+            val email = emailText?.text.toString()
+            val password = passwordText?.text.toString()
+            if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+                auth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
-                            Toast.makeText(this, "Usuario creado", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, "Usuario creado con éxito", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, Home::class.java)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                            startActivity(intent)
                         } else {
-                            Toast.makeText(baseContext, "Creacion fallida.", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(baseContext, "Creación fallida.", Toast.LENGTH_SHORT).show()
                         }
                     }
             }
         }
     }
-
-
 }
