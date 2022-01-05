26d22283d74760ba9e0f0b0d28588d40a252a427
xmariia55x <mgm7cns@uma.es>
2022-01-05 12:34:30 +0100

    Valoracion de un test

--- app/src/main/java/com/example/quizapp/activities/InfoTest.kt
+++ app/src/main/java/com/example/quizapp/activities/InfoTest.kt
@@ -2,25 +2,32 @@ package com.example.quizapp.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
+import android.widget.RatingBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
 import com.example.quizapp.R
 import com.example.quizapp.entities.SingletonMap
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-class InfoTest : AppCompatActivity() {
+class InfoTest : AppCompatActivity(), RatingBar.OnRatingBarChangeListener {
+    val test = SingletonMap["lastTest"] as DocumentSnapshot
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info_test)
 
-        val test = SingletonMap["lastTest"] as DocumentSnapshot
 
         findViewById<TextView>(R.id.label_titulo).text = test.get("titulo") as String
         findViewById<TextView>(R.id.label_descripcion).text = test.get("descripcion") as String
         findViewById<TextView>(R.id.label_categorias).text = (test.get("categorias") as List<String>).joinToString(" ")
-
+        findViewById<TextView>(R.id.lbl_preguntas).text = (test.get("preguntas") as List<HashMap<String,*>>).size.toString()
         findViewById<Button>(R.id.realizar_test).setOnClickListener {
             SingletonMap["lista_preguntas"] = (test["preguntas"] as List<HashMap<String,*>>).map { pregunta ->
                 CrearTest.Pregunta(
@@ -38,12 +45,20 @@ class InfoTest : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-
+        val rBar = findViewById<RatingBar>(R.id.calificacion_test)
+        rBar.onRatingBarChangeListener = this
         val resultado = SingletonMap["resultado test"]
         if (resultado != null) {
             SingletonMap.remove("resultado test")
             val (aciertos, fallos) = resultado as Pair<Int, Int>
             Toast.makeText(this, "Has acertado " + aciertos + " y has fallado " + fallos, Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onRatingChanged(p0: RatingBar?, p1: Float, p2: Boolean) {
+        Firebase.firestore.collection("tests")
+            .document(test.id).update("valoracion", p1).addOnSuccessListener {
+                Toast.makeText(this, "Test valorado con exito", Toast.LENGTH_SHORT).show()
+            }
     }
 }
