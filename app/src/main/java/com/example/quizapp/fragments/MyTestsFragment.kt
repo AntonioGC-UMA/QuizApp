5e2566428ed4d82a2bafd572093014f787b82e51
Sagrel <aoaee22@gmail.com>
2022-01-07 11:15:08 +0100

    Quitado codigo que venia de ejemplo pero no vamos a usar

--- app/src/main/java/com/example/quizapp/fragments/MyTestsFragment.kt
+++ app/src/main/java/com/example/quizapp/fragments/MyTestsFragment.kt
@@ -21,40 +21,16 @@ import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import java.util.*
 
 
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyTestsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyTestsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val tests = arguments?.getStringArrayList(ARG_NAME)
         val view = inflater.inflate(R.layout.fragment_my_tests, container, false)
         val buttonCreateTest = view.findViewById<FloatingActionButton>(R.id.createTestButton)
         buttonCreateTest.setOnClickListener {
@@ -110,20 +86,6 @@ class MyTestsFragment : Fragment() {
         return view
     }
 
-    companion object {
-        const val ARG_NAME = "tests"
-        fun newInstance(name: List<String>): MyTestsFragment {
-            val fragment = MyTestsFragment()
-            println(name)
-            val bundle = Bundle().apply {
-                putStringArrayList(ARG_NAME, name as ArrayList<String>)
-            }
-
-            fragment.arguments = bundle
-
-            return fragment
-        }
-    }
 
     // Esto es del recycler view
     inner class CustomAdapter(private val dataSet: List<DocumentSnapshot>) :
